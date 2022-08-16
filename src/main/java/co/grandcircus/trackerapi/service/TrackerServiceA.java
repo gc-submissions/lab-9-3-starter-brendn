c70f9528ec099afa2dacf185d0bd0bc900f9481b
brendn <brendn@users.noreply.github.com>
2022-08-15 20:53:29 -0400

    Added TrackerServiceA class

---
+++ src/main/java/co/grandcircus/trackerapi/service/TrackerServiceA.java
@@ -0,0 +1,82 @@
+package co.grandcircus.trackerapi.service;
+
+import co.grandcircus.trackerapi.model.CountPair;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TrackerServiceA implements TrackerService {
+
+    private List<CountPair> pairs = new ArrayList<>();
+
+    @Override
+    public void add(String token) {
+        int id = findPair(token);
+        if (id == -1) {
+            pairs.add(new CountPair(token, 1));
+        } else {
+            CountPair pair = pairs.get(id);
+            pair.setCount(pair.getCount() + 1);
+            pairs.set(id, pair);
+        }
+    }
+
+    @Override
+    public void reset() {
+        pairs.clear();
+    }
+
+    @Override
+    public int getTotalCount() {
+        int count = 0;
+        for (CountPair pair : pairs) {
+            count += pair.getCount();
+        }
+        return count;
+    }
+
+    @Override
+    public boolean getTokenExists(String token) {
+        return findPair(token) != -1;
+    }
+
+    @Override
+    public int getTokenCount(String token) {
+        int id = findPair(token);
+        if (id == -1) {
+            return 0;
+        }
+        return pairs.get(id).getCount();
+    }
+
+    @Override
+    public String getLatest() {
+        return null;
+    }
+
+    @Override
+    public CountPair getTop() {
+        return null;
+    }
+
+    @Override
+    public List<String> getLatest5() {
+        return null;
+    }
+
+    @Override
+    public List<CountPair> getTop5() {
+        return null;
+    }
+
+    private int findPair(String token) {
+        for (CountPair pair : pairs) {
+            if (pair.getToken().equals(token)) {
+                return pairs.indexOf(pair);
+            }
+        }
+        return -1;
+    }
+}
